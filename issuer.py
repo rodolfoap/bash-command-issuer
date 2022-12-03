a3b9620d17c5758dcf50c47e5cd35c6378b72825
RodolfoAP <rodolfo.alcazar@gtd.eu>
2022-12-03 07:46:10 +0100

    Better if OOP
    Change-Id: I6e0ab2afbe5a2071b19dcb69d2defda0d6aebdf0

--- issuer.py
+++ issuer.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
 import sys
-from pynput import keyboard
+from pynput import keyboard # Requires: pip install pynput
 
-def on_press(key):
-	try:
-		print('{0}'.format(key.char), end='')
-		sys.stdout.flush()
-	except AttributeError:
-		global line, lines
-		if key==keyboard.Key.pause:
-			print('>>> ', end='')
-			for key in lines[line]:
-				sender.press(key)
-				sender.release(key)
-			print()
-			line+=1
+# An [ENTER] keypress signal can be sent after writing each line
+# By default, better to disable it, so commands can be edited.
+SEND_ENTER=False
 
-			# ENTER can be sent:
-			#sender.press(keyboard.Key.enter)
-			#sender.release(keyboard.Key.enter)
-		else:
-			print('[{0}]'.format(key))
+class Issuer:
+	def __init__(this):
+		with open('commands.list') as file: this.lines=[line.rstrip() for line in file]
+		print("LINES READ:", this.lines)
+		this.line=0
+		this.sender=keyboard.Controller()
+		with keyboard.Listener(on_press=this.on_press) as listener: listener.join()
+	def on_press(this, key):
+		try:
+			print('{0}'.format(key.char), end='')
+			sys.stdout.flush()
+		except AttributeError:
+			if key==keyboard.Key.pause:
+				print('>>> ', end='')
+				for key in this.lines[this.line]:
+					this.sender.press(key)
+					this.sender.release(key)
+				print()
+				this.line+=1
+				if this.line==len(this.lines):
+					print('EOF reached.\n')
+					return False
 
-# INIT --------------------------------------------------------------------------
-with open('commandlist') as file: lines=[line.rstrip() for line in file]
-print("LINES READ:", lines)
-line=0
+				if SEND_ENTER:
+					this.sender.press(keyboard.Key.enter)
+					this.sender.release(keyboard.Key.enter)
+			else:
+				print('[{0}]'.format(key))
 
-sender=keyboard.Controller()
-with keyboard.Listener(on_press=on_press) as listener: listener.join()
+Issuer()
